2415c3bc561847da4700bacf981f2fa4f0547564
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2025-03-14 13:46:31 +0200

    feat: Add assertCount() to TestExecutionResult

--- src/Testing/TestExecutionResult.php
+++ src/Testing/TestExecutionResult.php
@@ -70,6 +70,15 @@ class TestExecutionResult extends ExecutionResult
 		return $this;
 	}
 
+	public function assertCount(int $count, string $key, string $field = null): self
+	{
+		$data = $this->data($field);
+
+		Assert::assertCount($count, data_get($data, $key));
+
+		return $this;
+	}
+
 	/**
 	 * @param callable|array<mixed, mixed> $expected
 	 */
