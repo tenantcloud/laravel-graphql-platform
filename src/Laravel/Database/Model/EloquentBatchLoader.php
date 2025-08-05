e8444d76d40a11c14beb4a4d6159ab442dfb48cc
Oleksandr Prypkhan <o.prypkhan@tenantcloud.com>
2025-08-05 17:52:43 +0200

    feat: Add more aggregations to EloquentBatchLoader

--- src/Laravel/Database/Model/EloquentBatchLoader.php
+++ src/Laravel/Database/Model/EloquentBatchLoader.php
@@ -4,6 +4,7 @@ namespace TenantCloud\GraphQLPlatform\Laravel\Database\Model;
 
 use Closure;
 use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Contracts\Database\Query\Expression;
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
@@ -28,7 +29,7 @@ class EloquentBatchLoader
 	 * @return Closure(): TReturn
 	 */
 	public function defer(
-		mixed $key,
+		ResolveKey|string $key,
 		Model $model,
 		callable $apply,
 		callable $map,
@@ -62,26 +63,159 @@ class EloquentBatchLoader
 	 * @return Closure(): int
 	 */
 	public function deferCount(
-		mixed $key,
+		ResolveKey|string $key,
 		Model $model,
 		string $relation,
 		?callable $callback = null
 	): Closure {
-		$alias = "{$relation}_count";
+		return $this->deferAggregate(
+			key: $key,
+			model: $model,
+			relation: $relation,
+			column: '*',
+			function: 'count',
+			map: fn (mixed $aggregate) => (int) $aggregate,
+			callback: $callback,
+		);
+	}
+
+	/**
+	 * @return Closure(): float
+	 */
+	public function deferMax(
+		ResolveKey|string $key,
+		Model $model,
+		string $relation,
+		string|Expression $column,
+		?callable $callback = null
+	): Closure {
+		return $this->deferAggregate(
+			key: $key,
+			model: $model,
+			relation: $relation,
+			column: $column,
+			function: 'max',
+			map: fn (mixed $aggregate) => (float) $aggregate,
+			callback: $callback,
+		);
+	}
+
+	/**
+	 * @return Closure(): float
+	 */
+	public function deferMin(
+		ResolveKey|string $key,
+		Model $model,
+		string $relation,
+		string|Expression $column,
+		?callable $callback = null
+	): Closure {
+		return $this->deferAggregate(
+			key: $key,
+			model: $model,
+			relation: $relation,
+			column: $column,
+			function: 'min',
+			map: fn (mixed $aggregate) => (float) $aggregate,
+			callback: $callback,
+		);
+	}
+
+	/**
+	 * @return Closure(): float
+	 */
+	public function deferSum(
+		ResolveKey|string $key,
+		Model $model,
+		string $relation,
+		string|Expression $column,
+		?callable $callback = null
+	): Closure {
+		return $this->deferAggregate(
+			key: $key,
+			model: $model,
+			relation: $relation,
+			column: $column,
+			function: 'sum',
+			map: fn (mixed $aggregate) => (float) $aggregate,
+			callback: $callback,
+		);
+	}
+
+	/**
+	 * @return Closure(): float
+	 */
+	public function deferAvg(
+		ResolveKey|string $key,
+		Model $model,
+		string $relation,
+		string|Expression $column,
+		?callable $callback = null
+	): Closure {
+		return $this->deferAggregate(
+			key: $key,
+			model: $model,
+			relation: $relation,
+			column: $column,
+			function: 'avg',
+			map: fn (mixed $aggregate) => (float) $aggregate,
+			callback: $callback,
+		);
+	}
+
+	/**
+	 * @return Closure(): bool
+	 */
+	public function deferExists(
+		ResolveKey|string $key,
+		Model $model,
+		string $relation,
+		string|Expression $column,
+		?callable $callback = null
+	): Closure {
+		return $this->deferAggregate(
+			key: $key,
+			model: $model,
+			relation: $relation,
+			column: $column,
+			function: 'exists',
+			map: fn (mixed $aggregate) => (bool) $aggregate,
+			callback: $callback,
+		);
+	}
+
+	/**
+	 * @template TModel of Model
+	 * @template TReturn
+	 *
+	 * @param callable(mixed, TModel): TReturn $map
+	 *
+	 * @return Closure(): TReturn
+	 */
+	public function deferAggregate(
+		ResolveKey|string $key,
+		Model $model,
+		string $relation,
+		string|Expression $column,
+		string $function,
+		callable $map,
+		?callable $callback = null,
+	): Closure {
+		$alias = "{$relation}_{$function}";
 
 		if ($callback !== null) {
 			$alias .= '_' . self::keyHash($key);
 		}
 
+		$callback ??= fn (Builder $query) => $query;
+
 		return $this->defer(
 			$key,
 			$model,
-			fn (Builder $query) => $callback ?
-				$query->withCount([
-					"{$relation} as {$alias}" => $callback,
-				]) :
-				$query->withCount("{$relation} as {$alias}"),
-			fn (Model $model) => (int) $model->{$alias}
+			fn (Builder $query) => $query->withAggregate([
+				"{$relation} as {$alias}" => $callback,
+			], $column, $function),
+			fn (Model $model) => $map($model->{$alias}, $model)
 		);
 	}
 
@@ -89,21 +223,26 @@ class EloquentBatchLoader
 	 * @return Closure(): EloquentCollection<int, Model>
 	 */
 	public function deferWith(
-		mixed $key,
+		ResolveKey|string $key,
 		Model $model,
 		string $relation,
 		?callable $callback = null
 	): Closure {
+		$callback ??= fn (Builder $query) => $query;
+
 		return $this->defer(
 			$key,
 			$model,
-			fn (Builder $query) => $callback ?
-				$query->with([
-					$relation => $callback,
-				]) :
-				$query->with($relation),
+			fn (Builder $query) => $query->with([
+				$relation => $callback,
+			]),
 			fn (Model $model) => $model->{$relation}
 		);
+	}
+
+	public static function keyHash(ResolveKey|string $key): string
+	{
+		return $key instanceof ResolveKey ? $key->hash() : md5($key);
 	}
 
 	/**
@@ -178,10 +317,5 @@ class EloquentBatchLoader
 		return $modelPrototype
 			->newModelQuery()
 			->whereKey($ids);
-	}
-
-	private static function keyHash(mixed $key): string
-	{
-		return $key instanceof ResolveKey ? $key->hash() : md5(serialize($key));
 	}
 }
